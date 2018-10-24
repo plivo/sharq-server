e9a8afe96b418aadf0c7ef8abf689005f95c3cf7
Tarun <tarun@Taruns-MacBook-Air.local>
2018-10-24 18:48:01 +0530

    SMS-632 fixes for running consul with supervisor and nginx

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -11,6 +11,5 @@ echo "[ENTRYPOINT] - Starting SharQServer."
 /usr/sbin/consul-template \
     -consul-addr "$CONSUL" \
     -template "/etc/sharq-server/config/sharq.conf.ctmpl:/etc/sharq-server/config/sharq.conf" \
-    -template "/etc/sharq-server/config/sharq.ini.ctmpl:/etc/sharq-server/config/sharq.ini"
-    -consul-retry-attempts=0 \
-    -exec "/opt/sharq-server/bin/uwsgi --ini /etc/sharq-server/config/sharq.ini && nginx -g 'daemon off;'"
+    -template "/etc/sharq-server/config/sharq.ini.ctmpl:/etc/sharq-server/config/sharq.ini" \
+    -exec "supervisord -c /etc/supervisord.conf"
