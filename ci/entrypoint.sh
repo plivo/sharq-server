99c5fc40176a9c1c05e079257cb8c166eb3528d6
qyunal <s.kunal@plivo.com>
2019-03-15 15:57:41 +0530

    sharq-server entry point changes

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -18,4 +18,8 @@ echo "
     -consul-addr "$CONSUL" \
     -template "/etc/sharq-server/config/sharq.conf.ctmpl:/etc/sharq-server/config/sharq.conf" \
     -template "/etc/sharq-server/config/sharq.ini.ctmpl:/etc/sharq-server/config/sharq.ini" \
-    -exec "supervisord -c /etc/supervisord.conf"
+    -consul-retry-attempts=0 -once
+
+echo "All templates are rendered. Starting sharq-server..."
+
+supervisord -c /etc/supervisord.conf
