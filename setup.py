bdf4bc5fee02655165d726cb5307bb178ee9acb4
Tarun <tarun@plivo.com>
2019-07-25 17:40:59 +0530

    changes for sharq core lib

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     packages=['sharq_server'],
     py_modules=['runner'],
     install_requires=[
-        'SharQ==0.5.1',
         'Flask==0.10.1',
         'Jinja2==2.7.2',
         'MarkupSafe==0.23',
