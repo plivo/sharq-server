a3c7fb5328b3717ae6b9b80b1e1f7d421bbc4aaf
varshit97plivo <varshit.battu@plivo.com>
2020-11-10 12:48:39 +0530

    add logs

--- sharq_server/server.py
+++ sharq_server/server.py
@@ -104,16 +104,20 @@ class SharQServer(object):
         }
         try:
             response = self.sq.dequeue(**request_data)
-            print("sharqvarshit::", response)
+            print("sharqvarshit::dequeueresponse", response)
+            try:
+                print("sharqvarshit::jsonify", response)
+            except Exception as e:
+                print("sharqvarshit::jsonifyexception", e)
             if response['status'] == 'failure':
                 return jsonify(**response), 404
         except Exception as e:
-            print("sharqvarshit::", e)
+            print("sharqvarshit::dequeueexception", e)
             import traceback
             for line in traceback.format_exc().splitlines():
                 print(line)
             response['message'] = e.message
-            print("sharqvarshit::", response)
+            print("sharqvarshit::dequeueexceptionresponse", response)
             return jsonify(**response), 400
 
         return jsonify(**response)
