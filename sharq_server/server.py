f21c06660182e4ec6370c68f9c5d17e7268de243
ajay-plivo <ajaykumar.verma@plivo.com>
2023-07-03 15:30:54 +0530

    VT-5650_sharq_qa

--- sharq_server/server.py
+++ sharq_server/server.py
@@ -119,7 +119,6 @@ class SharQServer(object):
 
         enqueue_allow, current_queue_length = self.sq.get_queue_length(queue_type, queue_id, max_queued_length)
         print("current_queue_length sharq-server ", current_queue_length)
-        print("enqueue_allow -->", type(enqueue_allow))
         if enqueue_allow:
             print('Inside enqueue_allow function....')
             try:
